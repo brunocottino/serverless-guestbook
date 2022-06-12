6bf1cdb12aa163193ed1f093d2e0530ca600b1da
brunocottino <105580098+brunocottino@users.noreply.github.com>
2022-06-12 12:47:37 +0200

    Update guestbook.js

--- guestbook.js
+++ guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = '';
+const apiUrl = 'https://d55fdb20.us-south.apigw.appdomain.cloud/guestbook';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
